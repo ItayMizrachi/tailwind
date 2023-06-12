9f30c5965c58ff1b18c41f67cdca06bcd3337c83
ItayMizrachi <itaymiz98@gmail.com>
2023-06-12 10:12:52 +0300

    tailwind - start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App text-center">
+      <h1 className="bg-red-900">React - Vite - TailWind</h1>
+      <h2 className="flex justify-center shadow-slate-600">Omri</h2>
+      <button className="bg-gradient-to-l from-blue-400 to-blue-700">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
